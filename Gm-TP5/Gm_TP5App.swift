2928ba7343785ae7ed9f7d85e009ca79421078a0
Max Prudhomme <account@maxprudhomme.com>
2026-01-19 14:37:21 +0100

    Initial Commit

---
+++ Gm-TP5/Gm_TP5App.swift
@@ -0,0 +1,17 @@
+//
+//  Gm_TP5App.swift
+//  Gm-TP5
+//
+//  Created by Max PRUDHOMME on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Gm_TP5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
